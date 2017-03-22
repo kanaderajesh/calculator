6c107d11bb76fe227c0ffe09d9b8da8da2e33d0d
rajesh chandrakant kanade <rkanade@rajeshs-MacBook-Pro.local>
2017-03-22 10:44:10 +0530

    sample plan json added

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-Import JSON file in Typescript
-
-Article here: https://hackernoon.com/import-json-into-typescript-8d465beded79#.uuabhfg3j
+Calculator API
 
 1. Download it. 
 2. Run `npm install`
--- app.ts
+++ app.ts
@@ -1,4 +1,26 @@
-import * as data from './example.json';
+import * as plans from './plans/815.json';
 
-const word = (<any>data).name;
-console.log(word); // output 'testing'
+const plan = (<any>(plans));
+
+
+let input = {
+    name : "rajesh",
+    sa : 500000,
+    age : 18,
+    term : 21,
+    plan : 815,
+    dab : true
+}
+
+const saRebate = (high, per ) => {
+
+}
+const modeRebate = (perm, per, mode) => {
+
+}
+const calculatePrem = (input) => {
+    let selectedPlan = plan[input.plan];
+    console.log('selected plan : ', selectedPlan.plan);
+}
+let jsonOutput = calculatePrem(input);
+console.log('Premium details : ' , jsonOutput);
